4b3fcc8d4bb1b9e98edfa85ad5a2391202aedb23
Rciat05 <rciglesiaa05@gmail.com>
2025-05-30 14:51:05 -0600

    agregando solo la descripcion

--- Clase # 6/6.2.1.py
+++ Clase # 6/6.2.1.py
@@ -1,5 +1,16 @@
-# Manejo de listas.
+'''
+Clase:        Fundamentos de Programacion G2
+Tema:         Manejo de listas
+Ejercicio:    6.2.2
+Descripción:  Manejo de valores repetidos
 
+Autor:        Roberto Carlos Iglesias Álvarez
+Fecha:        2025-05-30
+Estado:       [Terminado]
+'''
+
+
+# Manejo de listas.
 ValoresEntrada = input("Ingrese los numeros de entrada separado por espacios: ")
 lista = ValoresEntrada.split()
 lista_NoRepetidos = []
